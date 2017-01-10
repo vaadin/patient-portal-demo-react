c972606fe79bf0c897a168b5111edaa3b70f2c83
Sami Suo-Heikki <samiheikki@vaadin.com>
2017-01-10 16:41:00 +0200

    Add log out

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
+import LoginActions from './actions/LoginActions';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
   }
-
+  logOut() {
+    LoginActions.logoutUser();
+  }
   render() {
     return (
       <div className="container">
         <nav className="menu">
           <Link to={`/patients`}>Patients</Link>
           <Link to={`/analytics`}>Patients</Link>
-          <Link to={`/login`} className="right"><i className="fa fa-sign-out"></i> Logout</Link>
+          <Link to={`/login`} className="right" onClick={this.logOut}><i className="fa fa-sign-out"></i> Logout</Link>
         </nav>
         <div className="content">
           {this.props.children}
